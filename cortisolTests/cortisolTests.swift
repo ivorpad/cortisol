0dcbc0d9a93aa099b8ee060a574a059f835d50ad
gradion-io <ivor@gradion.io>
2026-02-13 09:51:31 +0100

    Initial Commit

---
+++ cortisolTests/cortisolTests.swift
@@ -0,0 +1,17 @@
+//
+//  cortisolTests.swift
+//  cortisolTests
+//
+//  Created by Ivor Padilla on 13/2/26.
+//
+
+import Testing
+@testable import cortisol
+
+struct cortisolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
